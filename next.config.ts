1346dc023f88abbd1c6b84856553f9182f6f2848
Yuliia <50263234+Yuliia010@users.noreply.github.com>
2026-05-28 00:13:51 +0200

    image fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "standalone",
     images: {
+        unoptimized: process.env.NODE_ENV !== "production",
         remotePatterns: [
             {
                 protocol: "http",

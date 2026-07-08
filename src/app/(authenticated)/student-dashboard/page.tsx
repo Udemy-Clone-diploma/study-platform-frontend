329e9017fa005c4fce4fd1b860b305f53328819f
Yuliia <50263234+Yuliia010@users.noreply.github.com>
2026-07-08 23:54:03 +0200

    schedule notific added

--- src/app/(authenticated)/student-dashboard/page.tsx
+++ src/app/(authenticated)/student-dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import { DashboardOverview } from "@/widgets/dashboard/DashboardOverview";
 
 export default function StudentDashboardPage() {
-  return <DashboardOverview role="student" />;
+  return (
+    <Suspense>
+      <DashboardOverview role="student" />
+    </Suspense>
+  );
 }

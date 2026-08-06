f3987aba1ae577015f3012b4b14b0b0271fa8e1b
MiuselmianMV <m.maksim2603@icloud.com>
2026-08-06 17:57:35 +0300

    fixed the background

--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -1,16 +1,14 @@
 import { Header } from "@/widgets/header";
 import { Footer } from "@/widgets/footer";
 
-export default function PublicLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <main className="flex flex-1 flex-col pt-16 lg:pt-0">{children}</main>
-            <Footer />
-        </div>
-    );
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex flex-1 flex-col pt-(--mobile-header-content-offset) lg:pt-0">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
 }
